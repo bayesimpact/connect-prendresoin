caa35165ab8cb500a57bfe266f6176dca7412283
Jérémie Doucy <jdoucy@gmail.com>
2025-12-19 18:36:46 +0100

    feat: force light mode

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -6,7 +6,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-startReactDsfr({ defaultColorScheme: 'system', useLang: () => 'fr' })
+startReactDsfr({ defaultColorScheme: 'light', useLang: () => 'fr' })
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
